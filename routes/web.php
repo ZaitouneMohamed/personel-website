afd1bf93808865de945ef033cd5ce45712ce3c8d
zaitoune <mohamedztn3@gmail.com>
2022-11-07 11:39:54 +0100

    some changes

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/admin', function () {
 })->name('admin.home');
 Route::resource('admin/settings', manageadminController::class);
 
-Route::prefix('hh')->group(function(){
+Route::prefix('admin')->group(function(){
     // Route::get('/', function () {
     //     return view('admin.home');
     // })->name('admin.home');
