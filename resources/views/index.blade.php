557282c329ecaa836522394bff8450a03bdb3ac1
zaitoune <mohamedztn3@gmail.com>
2022-11-07 15:51:27 +0100

    some changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="profile/{{$parametre->nav_logo}}"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" /
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
     <a href="https://www.flaticon.com/free-icons/email" title="email icons"></a>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
-    <title>Document</title>
+    <title>zaitoune</title>
 </head>
 <body>
     <header class="header" id="header">
